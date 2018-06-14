b9394078677b90203fe37f66cb73608432bc24d7
Calin Segarceanu <csegarceanu@gmail.com>
2018-06-13 21:00:00 -0500

    Test alphavantage api

--- stockmarket/routes/index.js
+++ stockmarket/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise-native');
+
+var stockData = function(req, res, next){
+  const options = {
+    uri: 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=MGYEGLIHVOSNG1H2',
+    headers:{
+      'User-Agent': 'Request-Promise'
+    }
+  };
+  request(options).then(function(data){
+    //req.data = JSON.parse(data);
+    req.data = data;
+    next();
+  });
+}
+
+router.use(stockData);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Stock Market Data', data: req.data });
 });
 
 module.exports = router;
